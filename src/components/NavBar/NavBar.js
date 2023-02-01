50ff7ec0bbe6136fb7350d858fef4b5c36239c5a
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-01 16:22:48 -0600

    Reorganized class 'logo' in NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,13 +6,14 @@ const NavBar = () => {
         <div className='container-fluid'>
             <div className='NavBar'>
                 <div className='logo'>
-                    <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['home']}>
-                        <Menu.Item key='home'>Home</Menu.Item>
-                        <Menu.Item key='about'>About</Menu.Item>
-                        <Menu.Item key='portfolio'>Portfolio</Menu.Item>
-                        <Menu.Item key='links'>Links</Menu.Item>
-                    </Menu>
+                    <a href='http://www.google.com'>ND</a>
                 </div>
+                <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['home']}>
+                    <Menu.Item key='home'>Home</Menu.Item>
+                    <Menu.Item key='about'>About</Menu.Item>
+                    <Menu.Item key='portfolio'>Portfolio</Menu.Item>
+                    <Menu.Item key='links'>Links</Menu.Item>
+                </Menu>
             </div>
         </div>
       );
