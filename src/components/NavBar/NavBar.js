2f11fa79edf4c1abf27064ab6fe6e9368e38c261
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-01 22:56:41 -0600

    Introduced inline styling to logo div in NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,15 +2,17 @@ import { Menu } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
 
+// Header also includes font-size for logo. Logo also has inline styling that will effect <Menu></Menu>
 const Header = styled.div`
     display: flex;
+    font-size: 40mm;
 `
 
 const NavBar = () => {
     return (
         <div className='container-fluid'>
             <Header>
-                <div className='logo'>
+                <div className='logo' style={{marginTop: '10mm',}}>
                     <a href='http://www.google.com'>ND</a>
                 </div>
                 <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['home']}>
