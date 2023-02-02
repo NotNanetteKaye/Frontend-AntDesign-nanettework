af3afe7f46df6546f52066021770e70548c267b4
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-01 23:24:26 -0600

    arreglé el diseño de NavBar en NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,8 +4,10 @@ import styled from 'styled-components';
 
 // Header also includes font-size for logo. Logo also has inline styling that will effect <Menu></Menu>
 const Header = styled.div`
-    display: flex;
     font-size: 40mm;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    display: flex;
 `
 
 const NavBar = () => {
@@ -15,7 +17,7 @@ const NavBar = () => {
                 <div className='logo' style={{marginTop: '10mm',}}>
                     <a href='http://www.google.com'>ND</a>
                 </div>
-                <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['home']}>
+                <Menu style={{color: 'pink', fontSize: '10mm', marginTop:'6mm',}} mode='horizontal' defaultSelectedKeys={['home']}>
                     <Menu.Item key='home'>Home</Menu.Item>
                     <Menu.Item key='about'>About</Menu.Item>
                     <Menu.Item key='portfolio'>Portfolio</Menu.Item>
